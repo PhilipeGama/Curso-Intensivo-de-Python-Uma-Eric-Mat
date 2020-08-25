211119475b5a2fb208215b90dbb349569b5a3391
philipe <philipegama97@gmail.com>
2020-08-24 21:19:29 -0400

    novos conteúdos

---
+++ 8 - Funções/8.2.py
@@ -0,0 +1,4 @@
+def favorite_book(title):
+	print("Um dos meus livros favoritos " + title.title() + ".")
+
+favorite_book("o poder do hábito")
---
+++ 8 - Funções/pets_1.py
@@ -0,0 +1,8 @@
+def descrive_pet(animal_type, pet_name):
+	"""Exibe informações sobre um animal de estimação."""
+	print("\nI have a " + animal_type + ".")
+	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+descrive_pet("hamster","harry")
+descrive_pet("dog","willie")
+descrive_pet("harry","hamster")
